a1de61ebfd51661f22190f7cc37f76f5c1668233
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-08-22 16:02:37 -0700

    redux - getState & dispatch

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
-import React from 'react';
+import React, { Component } from "react";
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
 
-class BankAccount extends React.Component {
-  state = {
-    accountBalance: 25.0,
-    incrementValue: 0
-  };
-
-  increment() {
-    console.log("executed!");
-    this.setState(
-      {
-        ...this.state,
-        accountBalance: this.state.accountBalance + this.state.incrementValue
-      }
-    );
-  };
-
-  render() {
-    return (
-      <div>
-        <h3>Account Balance: ${this.state.accountBalance}</h3>
-        <input
-          type="number"
-          onChange={event =>
-            this.setState({ incrementValue: +event.target.value })}
-          value={this.state.incrementValue}
-        />
-        <button onClick={this.increment.bind(this)}>
-          Increase Amount
-        </button>
-      </div>
-    );
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+    return state + 1;
+    case 'DECREMENT':
+    return state - 1;
+    case 'RESET':
+    return state = 0;
+    default:
+    return state;
   }
 }
 
-ReactDOM.render(
-  <BankAccount />,
-  document.getElementById('root')
-)
+const store = createStore(reducer);
+
+const Counter = ({ value, onIncrement, onDecrement, onReset }) => (
+  <div>
+  <h1>{value}</h1>
+  <button onClick={onIncrement}>+</button>
+  <button onClick={onReset}>Reset</button>
+  <button onClick={onDecrement}>-</button>
+  </div>
+);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+    value={ store.getState() }
+    onIncrement={ () => store.dispatch({ type: 'INCREMENT' }) }
+    onDecrement={ () => store.dispatch({ type: 'DECREMENT'}) }
+    onReset={ ()=> store.dispatch({ type: 'RESET'}) }
+    />,
+    document.getElementById('root')
+  );
+};
+
+render();
+store.subscribe(render)
