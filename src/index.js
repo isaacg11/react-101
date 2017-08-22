6a39ddb9f75547c4af1e436dd9ff420469d22e71
isaacg11 <isaacgreywebdeveloper@gmail.com>
2017-08-22 12:42:40 -0700

    high order components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
+class Name extends React.Component {
+  render() {
+    return <span>{this.props.firstName} {this.props.lastName}</span>
+  }
+}
+
+class Connected extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, {this.props.name}!</h1>
+        <Name firstName={this.props.firstName} lastName={this.props.lastName} /> is {this.props.status}
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <div>
-    <Hello name="Nikki"/>
-    <Hello name="Michael"/>
-    <Hello name="Andrew"/>
-  </div>,
+  <Connected firstName="Margaret" lastName="Hamilton" status="Connected" />,
   document.getElementById('root')
 )
